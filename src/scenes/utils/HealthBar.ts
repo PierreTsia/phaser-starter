5595a1502144569cbf40f7e43ae370741d79f19a
Pierre_Tsia <pierre.tsiakkaros@gmail.com>
2022-12-07 01:46:44 +0100

    fixup! :fire: enemy patrol + collision + health bar

--- src/scenes/utils/HealthBar.ts
+++ src/scenes/utils/HealthBar.ts
@@ -5,24 +5,31 @@ export default class HealthBar {
   x: number;
   y: number;
   value: number;
+  scale: number = 1;
   size = {
     width: 80,
     height: 8,
   };
 
-  constructor(scene: GameScene, x: number, y: number, health: number) {
+  pixelPerHp: number;
+
+  constructor(
+    scene: GameScene,
+    x: number,
+    y: number,
+    health: number,
+    scale = 1.2
+  ) {
     this.bar = new Phaser.GameObjects.Graphics(scene);
     this.bar.setScrollFactor(0, 0);
     this.x = x;
     this.y = y;
+    this.scale = scale;
     this.value = health;
+    this.pixelPerHp = this.size.width / health;
 
     scene.add.existing(this.bar);
     this.draw(x, y);
-  }
-
-  get pixelPerHealth() {
-    return this.value / this.size.width;
   }
 
   update(health: number) {
@@ -31,22 +38,44 @@ export default class HealthBar {
     this.draw(x, y);
   }
 
-  draw(
-    x: number,
-    y: number,
-    width = this.size.width,
-    height = this.size.height
-  ) {
-    console.log(this.pixelPerHealth);
+  draw(x: number, y: number) {
+    const margin = 2;
+    const hpWidth =
+      this.value * this.pixelPerHp > margin
+        ? this.value * this.pixelPerHp
+        : margin;
     this.bar.clear();
+    // border
+    this.bar.fillStyle(0x677a3c);
+    this.bar.fillRect(
+      x,
+      y,
+      this.size.width + margin,
+      this.size.height + margin
+    );
+    // fill
+    this.bar.fillStyle(0x96b389);
+    this.bar.fillRect(
+      x + margin,
+      y + margin,
+      this.size.width - margin,
+      this.size.height - margin
+    );
+
+    // hp bar
     this.bar.fillStyle(this.getColor());
-    this.bar.fillRect(x, y, width * this.pixelPerHealth, height);
+    this.bar.fillRect(
+      x + margin,
+      y + margin,
+      hpWidth - margin,
+      this.size.height - margin
+    );
   }
 
   private getColor() {
-    if (this.pixelPerHealth > 0.5) {
-      return 0x00ff00;
-    } else if (this.pixelPerHealth > 0.25) {
+    if (this.value * this.pixelPerHp > 0.5 * this.size.width) {
+      return 0x56af36;
+    } else if (this.value * this.pixelPerHp > 0.25 * this.size.width) {
       return 0xffff00;
     } else {
       return 0xff0000;
